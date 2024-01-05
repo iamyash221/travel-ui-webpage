3d57b6b35e35d524673afa64444949b34ffb7dfa
Yash Chavda <yashchavda360@gmail.com>
2024-01-05 12:05:57 -0500

    layout of website

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import Image from "next/image";
+import Camp from "@/components/Camp";
+import Feature from "@/components/Feature";
+import GetApp from "@/components/GetApp";
+import Guide from "@/components/Guide";
+import Hero from "@/components/Hero";
 
 export default function Home() {
-  return <></>;
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Guide />
+      <Feature />
+      <GetApp />
+    </>
+  );
 }
